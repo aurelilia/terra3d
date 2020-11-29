06041efe3b69340be2610826f976c80cd659931e
Ellie Ang <git@angm.xyz>
2020-11-29 16:15:00 +0100

    Generater should implement InventoryMetadata

--- core/src/xyz/angm/terra3d/common/items/metadata/blocks/GeneratorMetadata.kt
+++ core/src/xyz/angm/terra3d/common/items/metadata/blocks/GeneratorMetadata.kt
@@ -1,6 +1,6 @@
 /*
  * Developed as part of the Terra3D project.
- * This file was last modified at 10/16/20, 6:10 PM.
+ * This file was last modified at 11/29/20, 4:13 PM.
  * Copyright 2020, see git repository at git.angm.xyz for authors and other info.
  * This file is under the GPL3 license. See LICENSE in the root directory of this repository for details.
  */
@@ -10,12 +10,16 @@ package xyz.angm.terra3d.common.items.metadata.blocks
 import xyz.angm.terra3d.client.resources.I18N
 import xyz.angm.terra3d.common.items.Inventory
 import xyz.angm.terra3d.common.items.metadata.IMetadata
+import xyz.angm.terra3d.common.items.metadata.InventoryMetadata
 
 /** Metadata on a energy generator. */
-class GeneratorMetadata : IMetadata {
+class GeneratorMetadata : IMetadata, InventoryMetadata {
 
     var fuel = Inventory(1)
     var energyStored = 0
+
+    override val pull get() = fuel
+    override val push get() = fuel
 
     override fun toString() = """
         ${I18N["meta.stored"]}: $energyStored
